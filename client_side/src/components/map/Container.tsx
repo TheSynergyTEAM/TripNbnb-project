67791abbcdf061f6c1493d33a5eebde31e08af85
n2ptune <n2ptune@outlook.com>
2020-12-14 02:30:38 +0900

    init components, sdk, style for map component

---
+++ client_side/src/components/map/Container.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledMap = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+const Container: React.FC = () => {
+  return <StyledMap id="map"></StyledMap>
+}
+
+export default Container
