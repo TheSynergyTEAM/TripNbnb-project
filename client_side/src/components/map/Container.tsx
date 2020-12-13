2fbe442b99979f9322cb5e9766de56a26366b27c
n2ptune <n2ptune@outlook.com>
2020-12-14 02:50:09 +0900

    typing kakao sdk object in window, setup map container components

--- client_side/src/components/map/Container.tsx
+++ client_side/src/components/map/Container.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const StyledMap = styled.div`
@@ -6,6 +7,22 @@ const StyledMap = styled.div`
 `
 
 const Container: React.FC = () => {
+  const [map, setMap] = useState(null)
+
+  useEffect(() => {
+    const mapContainer = document.getElementById('map')
+    const options = {
+      center: new window.kakao.maps.LatLng(33.234567, 126.232323),
+      level: 5
+    }
+
+    setMap(new window.kakao.maps.Map(mapContainer, options))
+
+    return () => {
+      setMap(null)
+    }
+  }, [])
+
   return <StyledMap id="map"></StyledMap>
 }
 
--- client_side/src/pages/Map.tsx
+++ client_side/src/pages/Map.tsx
@@ -1,7 +1,12 @@
 import { RouteComponentProps } from 'react-router-dom'
+import MapContainer from 'components/map/Container'
 
 const Map: React.FC<RouteComponentProps> = () => {
-  return <div>Map</div>
+  return (
+    <div className="page-map">
+      <MapContainer />
+    </div>
+  )
 }
 
 export default Map
