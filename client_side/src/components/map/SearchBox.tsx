33736f1f0bc462ddd8cb6f3776476c31a8392247
n2ptune <n2ptune@outlook.com>
2020-12-14 13:41:43 +0900

    add antd for styling component

--- client_side/src/components/map/SearchBox.tsx
+++ client_side/src/components/map/SearchBox.tsx
@@ -1,9 +1,32 @@
+import { Input } from 'antd'
 import styled from 'styled-components'
 
-const StyledBox = styled.div``
+const { Search } = Input
+
+const StyledBox = styled.div`
+  top: 2rem;
+  left: 2rem;
+  position: fixed;
+  max-width: 300px;
+  min-width: 300px;
+  background-color: white;
+  border: 1px solid #ccc;
+  z-index: 10;
+  padding: 1rem 0.5rem;
+`
 
 const SearchBox: React.FC = () => {
-  return <StyledBox />
+  const onSearch = (value: string) => console.log(value)
+
+  return (
+    <StyledBox>
+      <Search
+        placeholder="여행지, 관광 명소"
+        onSearch={onSearch}
+        style={{ width: '100%' }}
+      />
+    </StyledBox>
+  )
 }
 
 export default SearchBox
--- client_side/src/pages/Map.tsx
+++ client_side/src/pages/Map.tsx
@@ -1,9 +1,11 @@
 import { RouteComponentProps } from 'react-router-dom'
 import MapContainer from 'components/map/Container'
+import SearchBox from 'components/map/SearchBox'
 
 const Map: React.FC<RouteComponentProps> = () => {
   return (
     <div className="page-map">
+      <SearchBox />
       <MapContainer />
     </div>
   )
