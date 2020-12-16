843213465f04d2e8ab7b5f510e62f2b9f2578df6
HyungJun Goo <9h0jun1115@gmail.com>
2020-12-17 01:02:27 +0900

    feat add Social login function (Kakao) and use templates to test it works

--- server_side/users/urls.py
+++ server_side/users/urls.py
@@ -6,4 +6,6 @@ app_name = "users"
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.log_out, name="logout"),
+    path("login/kakao", views.kakao_login, name="kakao-login"),
+    path("login/kakao/callback", views.kakao_callback, name="kakao-callback"),
 ]
