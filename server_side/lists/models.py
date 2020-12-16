67ee020748caef7ece27b5320cbd210828d24452
yu-jin-song <yujinsong.tech@gmail.com>
2020-12-16 23:01:47 +0900

    lists/models.py updated

--- server_side/lists/models.py
+++ server_side/lists/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class MyPlace(models.Model):
+    user = models.ForeignKey(
+        "users.User", related_name="list_place_u", on_delete=models.CASCADE
+    )
+    place = models.ManyToManyField(
+        "places.Place", related_name="list_place_p"
+    )
+
+
+
+class MyReview(models.Model):
+    user = models.ForeignKey(
+        "users.User", related_name="list_review_u", on_delete=models.CASCADE
+    )
+    review = models.ForeignKey(
+        "reviews.Review", related_name="list_review_r", on_delete=models.CASCADE
+    )
