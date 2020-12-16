57bc2757bd3fed73b523f9ad58d56ee0e6f1ef58
n2ptune <n2ptune@outlook.com>
2020-12-17 04:34:40 +0900

    fix: responsive styled header

--- client_side/src/components/common/Header.tsx
+++ client_side/src/components/common/Header.tsx
@@ -1,24 +1,29 @@
 import styled from 'styled-components'
-import { Button } from 'antd'
+import { Button, Row, Col } from 'antd'
 import { purple } from '@ant-design/colors'
 import { useContext, useState } from 'react'
 import UserContext from 'context/User'
 import Login from 'event/Login'
 import Avatar from 'antd/lib/avatar/avatar'
 
-const StyledHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  padding: 1rem 0.5rem;
-`
-
 const Title = styled.div`
   font-size: 1.2rem;
   font-weight: bold;
   color: ${purple.primary};
 `
+
+const columns = {
+  outer: {
+    xs: 24,
+    md: 18,
+    lg: 12
+  },
+  inner: {
+    xs: 12,
+    md: 9,
+    lg: 6
+  }
+}
 
 const Header: React.FC = () => {
   const [openPopup, setOpenPopup] = useState(false)
@@ -33,28 +38,41 @@ const Header: React.FC = () => {
   }
 
   return (
-    <StyledHeader className="shadow-box">
-      <Title>{process.env.REACT_APP_PROJECT_NAME?.toUpperCase()}</Title>
-      {isLoggedIn ? (
-        <div>
-          <Avatar
-            {...(user?.properties.thumbnail_image
-              ? { src: user?.properties.thumbnail_image }
-              : {})}
-            size={40}
-          >
-            {user?.properties.thumbnail_image
-              ? ''
-              : user?.properties.nickname[0]}
-          </Avatar>
-        </div>
-      ) : (
-        <div className="login">
-          <Button onClick={handleLogin}>로그인</Button>
-          {openPopup && <Login />}
-        </div>
-      )}
-    </StyledHeader>
+    <Row
+      className="shadow-box"
+      justify="center"
+      align="middle"
+      style={{ padding: '1rem' }}
+    >
+      <Col {...columns.outer}>
+        <Row justify="space-between">
+          <Col {...columns.inner}>
+            <Title>{process.env.REACT_APP_PROJECT_NAME?.toUpperCase()}</Title>
+          </Col>
+          <Col {...columns.inner} style={{ textAlign: 'right' }}>
+            {isLoggedIn ? (
+              <div>
+                <Avatar
+                  {...(user?.properties.thumbnail_image
+                    ? { src: user?.properties.thumbnail_image }
+                    : {})}
+                  size={30}
+                >
+                  {user?.properties.thumbnail_image
+                    ? ''
+                    : user?.properties.nickname[0]}
+                </Avatar>
+              </div>
+            ) : (
+              <div className="login">
+                <Button onClick={handleLogin}>로그인</Button>
+                {openPopup && <Login />}
+              </div>
+            )}
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   )
 }
 
