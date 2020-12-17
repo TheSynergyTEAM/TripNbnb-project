09618e6e3fb30b1286fa79dff1b410be6b5a456c
n2ptune <n2ptune@outlook.com>
2020-12-17 13:58:09 +0900

    fix: styled header

--- client_side/src/components/common/Header.tsx
+++ client_side/src/components/common/Header.tsx
@@ -5,11 +5,14 @@ import { useContext, useState } from 'react'
 import UserContext from 'context/User'
 import Login from 'event/Login'
 import Avatar from 'antd/lib/avatar/avatar'
+import { NavLink } from 'react-router-dom'
+import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint'
 
 const Title = styled.div`
   font-size: 1.2rem;
   font-weight: bold;
   color: ${purple.primary};
+  display: inline-block;
 `
 
 const columns = {
@@ -21,13 +24,40 @@ const columns = {
   inner: {
     xs: 12,
     md: 9,
-    lg: 6
+    lg: 4
   }
+}
+
+const StyledNavWrapper = styled.ul`
+  margin: 0;
+  padding: 0 0 0 0.5rem;
+  list-style-type: none;
+`
+
+const NavWrapper: React.FC = ({ children }) => {
+  const breakpoints = useBreakpoint()
+
+  return (
+    <StyledNavWrapper
+      style={{ display: breakpoints.lg ? 'inline-block' : 'none' }}
+    >
+      {children}
+    </StyledNavWrapper>
+  )
+}
+
+const NavItem: React.FC<{ to: string; name: string }> = (props) => {
+  return (
+    <li>
+      <NavLink to={props.to}>{props.name}</NavLink>
+    </li>
+  )
 }
 
 const Header: React.FC = () => {
   const [openPopup, setOpenPopup] = useState(false)
   const { isLoggedIn, user } = useContext(UserContext)
+  const breakpoint = useBreakpoint()
 
   const handleLogin = () => {
     if (isLoggedIn) {
@@ -39,15 +69,24 @@ const Header: React.FC = () => {
 
   return (
     <Row
-      className="shadow-box"
       justify="center"
       align="middle"
-      style={{ padding: '1rem' }}
+      style={{ padding: '1rem', borderBottom: '1px solid #ddd' }}
     >
       <Col {...columns.outer}>
-        <Row justify="space-between">
+        <Row justify="space-between" align="middle">
           <Col {...columns.inner}>
-            <Title>{process.env.REACT_APP_PROJECT_NAME?.toUpperCase()}</Title>
+            <NavLink to="/">
+              <Title>{process.env.REACT_APP_PROJECT_NAME?.toUpperCase()}</Title>
+            </NavLink>
+          </Col>
+          <Col
+            {...columns.inner}
+            style={{ display: breakpoint.lg ? 'block' : 'none' }}
+          >
+            <NavWrapper>
+              <NavItem to="/map" name="Map" />
+            </NavWrapper>
           </Col>
           <Col {...columns.inner} style={{ textAlign: 'right' }}>
             {isLoggedIn ? (
--- client_side/src/pages/Main.tsx
+++ client_side/src/pages/Main.tsx
@@ -1,7 +1,14 @@
 import { RouteComponentProps } from 'react-router-dom'
+import { Layout } from 'antd'
 
 const Main: React.FC<RouteComponentProps> = () => {
-  return <div>Main</div>
+  return (
+    <Layout>
+      <Layout.Content style={{ padding: '1rem', height: '100%' }}>
+        Content
+      </Layout.Content>
+    </Layout>
+  )
 }
 
 export default Main
