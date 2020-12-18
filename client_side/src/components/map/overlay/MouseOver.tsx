cdefb9ac103f67ea0a88da97b691b27a2d1a648f
n2ptune <n2ptune@outlook.com>
2020-12-18 23:00:16 +0900

    fix: render overlay container before view overlay to map

--- client_side/src/components/map/overlay/MouseOver.tsx
+++ client_side/src/components/map/overlay/MouseOver.tsx
@@ -1,20 +1,42 @@
+import MapContext from 'context/Map'
 import Marker from 'context/Marker'
-import { useContext, useEffect } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
+import { renderToString } from 'react-dom/server'
 
 interface MouseOverComponentProps {
   [key: string]: any
 }
 
+const OverlayContainer: React.FC = () => {
+  return <div>Overlay Container</div>
+}
+
 const MouseOver: React.FC<MouseOverComponentProps> = (props) => {
   const { marker } = useContext(Marker)
+  const { map } = useContext(MapContext)
+  const [ctmOverlay, setCTMOverlay] = useState<any>(null)
 
   useEffect(() => {
     console.log('marker 생성')
 
+    const overlay = new daum.maps.CustomOverlay({
+      position: marker.getPosition(),
+      content: renderToString(<OverlayContainer />),
+      yAnchor: 0.7,
+      zIndex: 50
+    })
+
+    console.log(overlay)
+
+    // @: TODO
+    overlay.setMap(map as daum.maps.Map)
+    setCTMOverlay(overlay)
+
     return () => {
       console.log('marker 삭제')
+      setCTMOverlay(null)
     }
-  }, [marker])
+  }, [marker, map])
 
   return (
     <div
