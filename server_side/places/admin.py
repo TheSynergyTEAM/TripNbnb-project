68a2db6113f41569df13e7cc821577eb6ec36c92
yu-jin-song <yujinsong.tech@gmail.com>
2020-12-21 22:27:16 +0900

    places/admin.py updated

--- server_side/places/admin.py
+++ server_side/places/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from .models import Place
+
+@admin.register(Place)
+class CustomPlaceAdmin(admin.ModelAdmin):
+    """CUstom Place Admin"""
+
+    list_filter = (
+        "name",
+        "city",
+    )
+    list_display = (
+        "name",
+        "city",
+        "address",
+        "writer",
+    )
--- server_side/places/serializers.py
+++ server_side/places/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from . import models
 
-
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Place
