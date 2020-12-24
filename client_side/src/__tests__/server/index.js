36d8b5db876203f95926e854d126944713f8877b
n2ptune <n2ptune@outlook.com>
2020-12-24 16:56:26 +0900

    feat: view user reviews
    * fix: send review data from test server

--- client_side/src/__tests__/server/index.js
+++ client_side/src/__tests__/server/index.js
@@ -4,6 +4,19 @@ const app = express()
 const { port } = config
 
 const randRange = (min, max) => Math.floor(Math.random() * (max - min)) + min
+const ratings = []
+
+for (let i = 0; i <= 5; i += 0.5) {
+  ratings.push(i)
+}
+
+// 아무 말
+const lorem = [
+  '국회의원은 그 지위를 남용하여 국가·공공단체 또는 기업체와의 계약이나 그 처분에 의하여 재산상의 권리·이익 또는 직위를 취득하거나 타인을 위하여 그 취득을 알선할 수 없다.',
+  '국교는 인정되지 아니하며, 종교와 정치는 분리된다. 피고인의 자백이 고문·폭행·협박·구속의 부당한 장기화 또는 기망 기타의 방법에 의하여 자의로 진술된 것이 아니라고 인정될 때 또는 정식재판에 있어서 피고인의 자백이 그에게 불리한 유일한 증거일 때에는 이를 유죄의 증거로 삼거나 이를 이유로 처벌할 수 없다.',
+  '국무총리는 국무위원의 해임을 대통령에게 건의할 수 있다. 위원은 정당에 가입하거나 정치에 관여할 수 없다.',
+  '선거에 관한 경비는 법률이 정하는 경우를 제외하고는 정당 또는 후보자에게 부담시킬 수 없다.'
+]
 
 app.use(express.json())
 
@@ -21,6 +34,7 @@ app.get('/places/:id', (req, res) => {
   const thumbnailImageCount = randRange(0, 5)
   const images = []
   const thumbnailImages = []
+  const reviews = { data: [], meta: {} }
 
   for (let i = 0; i < randImageCount; i++) {
     images.push(`https://picsum.photos/80/80?random=${i}`)
@@ -30,6 +44,27 @@ app.get('/places/:id', (req, res) => {
     thumbnailImages.push(`https://picsum.photos/350/200?random=${i}`)
   }
 
+  const reviewCount = randRange(0, 11)
+
+  for (let i = 0; i < reviewCount; i++) {
+    // 리뷰 내용
+    const content = lorem[randRange(0, lorem.length)]
+    const rating = ratings[randRange(0, ratings.length)]
+    const user = {
+      avatar: `https://picsum.photos/30/30?random=${i}`,
+      name: 'blah blah'
+    }
+
+    reviews.data.push({ content, rating, user })
+  }
+
+  reviews.meta.review_average =
+    reviews.data.length &&
+    (
+      reviews.data.reduce((pre, cur) => (pre += cur.rating), 0) /
+      reviews.data.length
+    ).toFixed(1)
+
   setTimeout(() => {
     // send dummy data after a second
     res.send({
@@ -38,7 +73,7 @@ app.get('/places/:id', (req, res) => {
       review_count: fakeReviewCount(),
       images,
       thumbnailImages,
-      reviews: []
+      reviews
     })
   }, 1000)
 })
--- client_side/src/components/map/detail/Reviews.tsx
+++ client_side/src/components/map/detail/Reviews.tsx
@@ -1,20 +1,82 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import MarkerContext from 'context/Marker'
+import Section from './common/Section'
+import { SecondaryText, Title } from 'components/common/typography'
+import { List, Rate, Space, Typography } from 'antd'
+import Avatar from 'antd/lib/avatar/avatar'
+import styled from 'styled-components'
 
 interface ReviewsComponentProps {
-  reviews: any[]
+  reviews: {
+    data: any[]
+    meta?: any
+  }
 }
+
+const StyledListItemMeta = styled(List.Item.Meta)`
+  & .ant-list-item-meta-title {
+    margin-bottom: 0;
+  }
+`
 
 const Reviews: React.FC<ReviewsComponentProps> = ({ reviews }) => {
   const { detailItem } = useContext(MarkerContext)
+  const [isSpread, setIsSpread] = useState(false)
+  const slicedReviews = useCallback(() => {
+    if (!isSpread) {
+      return reviews.data.slice(0, 5)
+    } else {
+      return reviews.data
+    }
+  }, [isSpread, reviews.data])
+  const isOverflow = reviews.data.length > 5
 
   useEffect(() => {
-    if (detailItem) {
-      // console.log(detailItem)
+    if (reviews && detailItem) {
+      console.log(reviews, detailItem)
     }
-  }, [detailItem])
+  }, [reviews, detailItem])
 
-  return <div>reviews</div>
+  return (
+    <Section title={<Title level={5}>리뷰</Title>}>
+      {reviews.data.length ? (
+        <>
+          <List
+            dataSource={slicedReviews()}
+            itemLayout="vertical"
+            renderItem={(review) => (
+              <List.Item>
+                <StyledListItemMeta
+                  avatar={<Avatar src={review.user.avatar} size="default" />}
+                  title={review.user.name}
+                  description={
+                    <Rate
+                      disabled
+                      defaultValue={review.rating}
+                      allowHalf
+                      style={{ fontSize: '16px' }}
+                    />
+                  }
+                />
+                {review.content}
+              </List.Item>
+            )}
+          />
+          {isOverflow && (
+            <Space align="center" style={{ margin: '0 auto' }}>
+              <Typography.Link onClick={() => setIsSpread(!isSpread)}>
+                {isSpread
+                  ? '리뷰 접기'
+                  : `${reviews.data.length - 5}개 리뷰 모두 보기`}
+              </Typography.Link>
+            </Space>
+          )}
+        </>
+      ) : (
+        <SecondaryText>등록된 리뷰가 없습니다.</SecondaryText>
+      )}
+    </Section>
+  )
 }
 
 export default Reviews
