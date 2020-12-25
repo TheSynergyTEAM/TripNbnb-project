c3e4fa2aef54db22d2a6c1302facbe60df4575c2
n2ptune <n2ptune@outlook.com>
2020-12-25 14:29:45 +0900

    test: case for header components, modified test commands to ignore test local server

--- client_side/src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from '../App'
-
-test('renders default text', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Default App/i)
-  expect(linkElement).toBeInTheDocument()
-})
