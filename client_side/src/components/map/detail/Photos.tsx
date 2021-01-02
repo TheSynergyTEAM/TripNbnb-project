9548ccead27118aa51ee75870ee8e304d554e340
n2ptune <n2ptune@outlook.com>
2021-01-02 20:29:14 +0900

    fix: fixed height of photo column component
    * fix: send place name when request place data to server

--- client_side/src/components/map/detail/Photos.tsx
+++ client_side/src/components/map/detail/Photos.tsx
@@ -14,6 +14,7 @@ const PhotoCol: React.FC<any> = ({ image }) => {
         src={image}
         preview={false}
         width="100%"
+        height={100}
         style={{ borderRadius: '5px' }}
       />
     </Col>
