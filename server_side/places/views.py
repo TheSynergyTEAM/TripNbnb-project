eac01b17a05e0d7671c636903b20c987fd829cc1
n2ptune <n2ptune@outlook.com>
2021-01-02 17:20:17 +0900

    fix: get place objects with filter

--- server_side/places/views.py
+++ server_side/places/views.py
@@ -14,66 +14,70 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+
 class PlaceView(viewsets.ModelViewSet):
-    serializer_class = PlaceSerializer
-    queryset = models.Place.objects.all()
-    print()
+  serializer_class = PlaceSerializer
+  queryset = models.Place.objects.all()
+  print()
+
 
 @method_decorator(csrf_exempt, name="dispatch")
 def place_view(request, id):
-    place_json = {
-        "data" : [],
-        "images" : [],
-    }
-    place_name = request.GET.get("name")
-    try:
-        place = models.Place.objects.get(contentid=id)
-        reviews = place.reviews_p.all()
-        for review in reviews:
-            place_json["data"].append(
-                {
-                    "username" : str(review.user),
-                    "review" : str(review.review),
-                    "rating" : str(review.rating),
-                    "created" : str(review.created),
-                }
-            )
-    except models.Place.DoesNotExist:
-        pass
-    images = get_images(str(place_name))
-    for img in images:
-        place_json["images"].append(img["link"])
+  place_json = {
+      "data": [],
+      "images": [],
+  }
+  place_name = request.GET.get("name")
+  try:
+    places = models.Place.objects.filter(contentid=id)
+    for place in places:
+      reviews = place.reviews_p.all()
+      for review in reviews:
+        place_json["data"].append(
+            {
+                "username": str(review.user),
+                "review": str(review.review),
+                "rating": str(review.rating),
+                "created": str(review.created),
+            }
+        )
+  except models.Place.DoesNotExist:
+    pass
+  images = get_images(str(place_name))
+  for img in images:
+    place_json["images"].append(img["link"])
 
-    return JsonResponse(place_json)
+  return JsonResponse(place_json)
+
 
 def place_photos_view(request):
-    city = request.GET.get("city")
+  city = request.GET.get("city")
 
+  items = get_images(city)
+  city_json = {str(city): []}
+  for item in items:
+    img_link = item.get("link")
+    city_json.get(str(city)).append(img_link)
 
-    items = get_images(city)
-    city_json = {str(city):[]}
-    for item in items:
-        img_link = item.get("link")
-        city_json.get(str(city)).append(img_link)
+  return JsonResponse(city_json)
 
-    return JsonResponse(city_json)
 
 def get_images(keyword):
-    client_id = os.environ.get("NAVER_CLIENT_ID")
-    client_secret = os.environ.get("NAVER_CLIENT_SECRET")
-    encText = urllib.request.quote(keyword)
-    url = "https://openapi.naver.com/v1/search/image?query=" + encText + "&display=20"
-    url_request = urllib.request.Request(url)
-    url_request.add_header("X-Naver-Client-Id", client_id)
-    url_request.add_header("X-Naver-Client-Secret", client_secret)
-    response = urllib.request.urlopen(url_request)
-    
-    rescode = response.getcode()
-    if rescode == 200:
-        response_body = response.read()
-        received_json_data = json.loads(response_body.decode("utf-8"))
-        items = received_json_data.get("items")
-        return items
-    else:
-        print("Error Code:" +str(rescode))
-        return 
+  client_id = os.environ.get("NAVER_CLIENT_ID")
+  client_secret = os.environ.get("NAVER_CLIENT_SECRET")
+  encText = urllib.request.quote(keyword)
+  url = "https://openapi.naver.com/v1/search/image?query=" + encText + "&display=20"
+  url_request = urllib.request.Request(url)
+  url_request.add_header("X-Naver-Client-Id", client_id)
+  url_request.add_header("X-Naver-Client-Secret", client_secret)
+  response = urllib.request.urlopen(url_request)
+
+  rescode = response.getcode()
+  if rescode == 200:
+    response_body = response.read()
+    received_json_data = json.loads(response_body.decode("utf-8"))
+    items = received_json_data.get("items")
+    return items
+  else:
+    print("Error Code:" + str(rescode))
+    return
