4a7dfc5f82ece650b1ee2d53166bfe94924b7db2
n2ptune <n2ptune@outlook.com>
2021-01-03 22:25:36 +0900

    fix: commented overlay component fetch place data logic

--- client_side/src/components/map/overlay/Container.tsx
+++ client_side/src/components/map/overlay/Container.tsx
@@ -21,31 +21,31 @@ const Container: React.FC = (props) => {
     overlay.setContent(content)
     overlay.setMap(map as daum.maps.Map)
 
-    const fetchPlace = () => {
-      return new Promise((resolve, reject) => {
-        const fakeNumber = () => Math.floor(Math.random() * (100 - 1 + 1)) + 1
+    // const fetchPlace = () => {
+    //   return new Promise((resolve, reject) => {
+    //     const fakeNumber = () => Math.floor(Math.random() * (100 - 1 + 1)) + 1
 
-        axios
-          .get(`http://localhost:7999/places/${fakeNumber()}`)
-          .then((result) => resolve(result.data))
-          .catch((error) => reject(error))
-      })
-    }
+    //     axios
+    //       .get(`http://localhost:7999/places/${fakeNumber()}`)
+    //       .then((result) => resolve(result.data))
+    //       .catch((error) => reject(error))
+    //   })
+    // }
 
-    // 데이터 로드 후 오버레이 표시
-    fetchPlace()
-      .then((place: any) => {
-        const loadedDataContent = renderToString(
-          <Content
-            rating={place.rating}
-            reviewCount={place.review_count}
-            loading={false}
-            markerPlace={markerPlace}
-          />
-        )
-        overlay.setContent(loadedDataContent)
-      })
-      .catch((error) => console.error(error))
+    // // 데이터 로드 후 오버레이 표시
+    // fetchPlace()
+    //   .then((place: any) => {
+    //     const loadedDataContent = renderToString(
+    //       <Content
+    //         rating={place.rating}
+    //         reviewCount={place.review_count}
+    //         loading={false}
+    //         markerPlace={markerPlace}
+    //       />
+    //     )
+    //     overlay.setContent(loadedDataContent)
+    //   })
+    //   .catch((error) => console.error(error))
 
     return () => {
       // 마커 마우스 이벤트(mouseout) 발생 시 맵에 표시된 오버레이 제거
