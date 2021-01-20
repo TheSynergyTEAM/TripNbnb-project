004886f69fad5a2e82ef6808eb3b20e24d53cdab
n2ptune <n2ptune@outlook.com>
2021-01-20 16:33:11 +0900

    fix: send empty array if user does not have any list
    * fix: create list and send that created list

--- server_side/lists/views.py
+++ server_side/lists/views.py
@@ -11,7 +11,6 @@ from . import models
 from places import models as place_models
 from places import views as place_views
 
-
 # Create your views here.
 
 
@@ -20,33 +19,34 @@ class ListView(viewsets.ModelViewSet):
     queryset = models.List.objects.all()
     print()
 
-@method_decorator(csrf_exempt, name="dispatch")
+
+@method_decorator(csrf_exempt, name = "dispatch")
 def list_view(request, pk):
-    
-    user = users_models.User.objects.get(pk=pk)
-    user_list_json = {
-        "user" : str(user.username),
-        "places" : []
-    }
-    places = user.list.all()[0].places.all()
-    if len(places) > 0: 
-        for place in places:
-            user_list_json.get("places").append(
-                {
-                    "name" : str(place.name),
-                    "address" : str(place.address),
-                    "photos" : [image["link"] for image in place_views.get_images(str(place.name))]
-                }
-            )
-    
+
+    user = users_models.User.objects.get(pk = pk)
+    user_list_json = {"user": str(user.username), "places": []}
+    places = user.list.all()
+    if len(places) > 0:
+        for place in places[0].places.all():
+            user_list_json.get("places").append({
+                "name":
+                str(place.name),
+                "address":
+                str(place.address),
+                "photos": [
+                    image["link"]
+                    for image in place_views.get_images(str(place.name))
+                ]
+            })
+
     return JsonResponse(user_list_json)
 
 
-@method_decorator(csrf_exempt, name="dispatch")
+@method_decorator(csrf_exempt, name = "dispatch")
 def create_list(request):
     received_json_data = json.loads(request.body.decode("utf-8"))
     user_pk = received_json_data.get("userid")
-    user = users_models.User.objects.get(pk=user_pk)
+    user = users_models.User.objects.get(pk = user_pk)
     content = received_json_data.get("content")
     place_pk = content.get("placeId")
     place_name = content.get("placeName")
@@ -55,33 +55,37 @@ def create_list(request):
     place_mapy = content.get("mapy")
 
     try:
-        user_list = models.List.objects.get(user=user)
+        user_list = models.List.objects.get(user_id = user)
 
     except models.List.DoesNotExist:
         try:
-            place = place_models.objects.get(contentid=place_pk)
+            place = place_models.Place.objects.get(contentid = place_pk)
+
         except place_models.Place.DoesNotExist:
             place = place_models.Place.objects.create(
                 name = place_name,
                 contentid = place_pk,
                 address = place_address,
-                mapx= place_mapx,
-                mapy= place_mapy,
+                mapx = place_mapx,
+                mapy = place_mapy,
             )
         user_list = models.List.objects.create(
             name = f"{user.username}'s List",
             user = user,
         )
-    user_list.places.add(place)
-    user_list.save()
+    finally:
+        place = place_models.Place.objects.get(contentid = place_pk)
+        user_list.places.add(place)
+        user_list.save()
 
     user_list_json = {
         "username": str(user.username),
         "user_id": str(user_pk),
         "user_profile": str(user.profile_img.url),
-        "user_list" : []
+        "user_list": []
     }
-    for l in user_list.places.all():
-        user_list_json.get("user_list").append(l.name)
+    if user_list:
+        for l in user_list.places.all():
+            user_list_json.get("user_list").append(l.name)
 
     return JsonResponse(user_list_json)
