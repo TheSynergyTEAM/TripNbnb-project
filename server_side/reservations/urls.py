47a6d8d9822395bf53907f916c301ec2adc7ee34
yu-jin-song <yujinsong.tech@gmail.com>
2021-02-15 19:14:38 +0900

    Modified: reservation url

--- server_side/reservations/urls.py
+++ server_side/reservations/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "reservations"
 
 urlpatterns = [
-    path("place/<int:id>/", views.reservation_check, name = "select"),
+    path("place/check/", views.reservation_check, name = "select"),
     path("place/create/", views.reservation_confirm, name = "create"),
 #    path("update/<int:id>/", views.reservation_update, name = "update")
 #    path("delete/<int:id>/", views.reservation_cancel, name = "delete")
