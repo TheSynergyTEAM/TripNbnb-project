758ac2b0333c7181aa0963cf8c40089347a2bb5f
HyungJun Goo <9h0jun1115@gmail.com>
2021-03-01 01:32:45 +0900

    fix: settings database connection

--- server_side/config/settings.py
+++ server_side/config/settings.py
@@ -101,7 +101,7 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = {}
+    DATABASES = {'default': {}}
     db_from_env = dj_database_url.config(conn_max_age=500) 
     DATABASES['default'].update(db_from_env)
     django_heroku.settings(locals())
