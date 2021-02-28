876e186a518ea4c5c5cc778a0af95d21d6e0d00c
HyungJun Goo <9h0jun1115@gmail.com>
2021-03-01 01:52:07 +0900

    add: static settings

--- server_side/config/settings.py
+++ server_side/config/settings.py
@@ -147,6 +147,11 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 AUTH_USER_MODEL = "users.User"
 
 CORS_ORIGIN_WHITELIST = ["https://localhost:3000", 'https://tripbnb.netlify.app/']
