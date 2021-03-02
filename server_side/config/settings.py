d3b5d375a97b6c596deae2067672d7b9fb49776b
yu-jin-song <yujinsong.tech@gmail.com>
2021-03-03 06:58:27 +0900

    fix: settings.py for test

--- server_side/config/settings.py
+++ server_side/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "hi^du)n!jw(8-ihbjacbn-eu@*p0h^
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
 
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['https://tripnbnbserver.herokuapp.com', "https://127.0.0.1:8000", "https://localhost" ]
+ALLOWED_HOSTS = ['https://tripnbnbserver.herokuapp.com', "*"]
 
 
 # Application definition
